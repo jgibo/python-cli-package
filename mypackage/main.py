b303bd006968700383bc01467880dad34c5e3ed5
Jordan Gibbings <55154696+jgibo@users.noreply.github.com>
2022-08-28 10:25:10 +0800

    Update main.py

--- mypackage/main.py
+++ mypackage/main.py
@@ -4,5 +4,5 @@ def main():
     print('Hello python CLI!')
 
 if __name__ == '__main__':
-    # debugging/development code. Only run when program is run as 'python kube_secret_pirate/main.py', not when used as installed cli.
+    # debugging/development code. Only run when program is run as 'python mypackage/main.py', not when used as installed cli.
     print('Hello developer of CLI!')
